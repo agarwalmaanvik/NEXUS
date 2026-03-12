b0a639188670a411fb24e46b80aa57d132bef0ce
agarwalmaanvik <agarwal.maanvik@gmail.com>
2026-03-12 23:36:25 +0530

    Open access

--- web_gui/vite.config.js
+++ web_gui/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+    plugins: [react()],
+    server: {
+        port: 3000,
+        host: true,
+        allowedHosts: ['nexuspoker.live', 'api.nexuspoker.live'],
+        open: false
+    }
 })
